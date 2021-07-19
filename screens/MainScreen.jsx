2ea95035852ff7e199e5433e273f427cb10bbfe1
Betts <84404748+Betts@users.noreply.github.com>
2021-07-19 18:54:21 -0400

    Update MainScreen.jsx
    Added swipe functionality, image display, and like, dislike, and comment buttons.

--- screens/MainScreen.jsx
+++ screens/MainScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef} from "react";
 import {
   ActivityIndicator,
   Button,
@@ -10,10 +10,17 @@ import {
   TouchableOpacity,
 } from "react-native";
 
+
 import { getPersistantData } from "../context/Storage";
+import BottomBar from "../components/BottomBar";
+import Swipes from "../components/Swipes";
+
+let imagePath = require("../assets/placeholder3.jpg");
+
 
 export default function MainScreen({ navigation }) {
   const [token, setToken] = useState("");
+  const swipesRef = useRef(null);
 
   // Get Token and set state
   getPersistantData("token")
@@ -23,16 +30,35 @@ export default function MainScreen({ navigation }) {
     .catch((err) => console.error(err));
 
   return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Text>Token: {token}</Text>
-      <Button title="To Login" onPress={() => navigation.navigate("loginScreenStack", { screen: "Login" })} />
+    <View style={styles.container}>
+      <View style ={styles.swipes}> 
+                  <Swipes>
+                  </Swipes>
+      </View>
+      <View>
+        <BottomBar />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+  },
+
+  swipes: {
+    flex: 1,
+    padding: 10,
+    paddingTop: 8,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+    elevation: 7,
+  },
+})
+
