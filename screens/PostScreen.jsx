a5a585a3f7a99374b5b1adc03b501b7df8428ed9
ErickVerlangieri <erickv@knights.ucf.edu>
2021-07-31 01:44:55 -0400

    Update PostScreen.jsx

--- screens/PostScreen.jsx
+++ screens/PostScreen.jsx
@@ -8,6 +8,9 @@ import {
   StyleSheet,
   Image,
   TouchableOpacity,
+  FlatList,
+  SafeAreaView,
+  StatusBar,
 } from "react-native";
 import GestureRecognizer, {
   swipeDirections,
@@ -16,84 +19,155 @@ import GestureRecognizer, {
 import { getPersistantData } from "../context/Storage";
 import { setPersistantData } from "../context/Storage";
 
-const config = {
-  velocityThreshold: 0.3,
-  directionalOffsetThreshold: 80,
-};
-
 export default function PostScreen({ navigation }) {
-  const onSwipeUp = (gestureState) => {
-    setPersistantData("newComments", "1");
-    navigation.navigate("mainScreenStack", { screen: "Comments" });
-  };
 
-  const [token, setToken] = useState("");
-
-  getPersistantData("token")
-    .then((result) => {
-      setToken(result);
-    })
-    .catch((err) => console.error(err));
-
+  const ARRAY = [
+    {
+		picture: '',
+		id: '',
+	},  
+  ];
+  
+  const loadPosts = async (last) => {
+    try {
+      let requestBody = {
+        query: `
+                query { 
+				userPosts{ 
+				_id 
+				poster{ _id login email verified } 
+				title 
+				Image 
+				Content 
+				upvotes 
+				downvotes 
+				userVoteStatus} 
+				}
+            `,
+      };
+	
+      const response = await fetch("http://largeproject.herokuapp.com/api", {
+            method: "POST",
+            body: JSON.stringify(requestBody),
+            headers: { "Content-Type": "application/json", "Authorization": "Bearer " + last},
+          });
+		  
+      if (response.status !== 200 && response.status !== 201) {
+        throw new Error("Something happened on our end, try again later!");
+      }
+	  
+      const result = await JSON.parse(await response.text());
+      const length = result.data.userPosts.length;
+      for (let i=0; i<length; i++) {   
+		ARRAY.push({picture: result.data.userPosts[i].Image, id: result.data.userPosts[i]._id});
+      }
+      //not returning a token
+      setPersistantData("token", result.extensions.token);
+    } catch (error) {
+        console.log(error.message);
+      }   
+  }
+	  getPersistantData("token")
+	  .then((result) => {
+		loadPosts(result);
+	  })
+	  .catch((err) => console.error(err));
+				
   return (
-    // Container View
-    <GestureRecognizer
-      onSwipeUp={(state) => onSwipeUp(state)}
-      config={config}
-      style={{
-        flex: 1,
-      }}
-    >
-      <View>
-        <Text>Post Screen</Text>
-        <Text>TOKEN: {token}</Text>
-      </View>
-    </GestureRecognizer>
+    <View style={styles.container}>
+      <FlatList
+        data={ARRAY}
+        renderItem={({item}) => (
+		<Image source={{uri:item.picture}}  style={{width:60, height:60,borderRadius:4}} />
+		)}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#fff",
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+   flex: 1,
+   paddingTop: 22
   },
-  image: {
-    marginBottom: 40,
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  row: {
+    //backgroundColor: "#fff",
+    flexDirection: "row"
+    //flex: 1,
+    //alignItems: "center",
+    //justifyContent: "center",
   },
   inputView: {
-    backgroundColor: "#00ff",
-    borderRadius: 30,
-    width: "70%",
-    height: 45,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    width: "65%",
+    height: 135,
     marginBottom: 20,
     alignItems: "center",
   },
   TextInput: {
-    height: 50,
+    height:80,
     flex: 1,
     padding: 10,
-    marginLeft: 20,
+    marginTop: 25,
+    marginBottom: 10,
+    marginLeft: 10,
   },
-  forgot_button: {
-    height: 30,
-    marginBottom: 30,
-  },
-  loginBtn: {
-    width: "80%",
+  commentBtn: {
+    width: "35%",
     borderRadius: 25,
-    height: 50,
+    height: 80,
     alignItems: "center",
-    justifyContent: "center",
-    marginTop: 40,
+  //justifyContent: "center",
+    marginTop: 25,
+    marginBottom: 10,
+    marginRight: 10,
     backgroundColor: "#007",
   },
-  loginText: {
+  commentText: {
     color: "white",
+    alignItems: "center",
   },
   errorMessage: {
     color: "red",
     marginBottom: 10,
   },
+  commentSpace : {
+    height: "20%",
+  },
+  comment: {
+    padding: 10,
+  },
+  name: {
+    color: "black",
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+  date: {
+    color: "grey",
+    fontSize: 13,
+    //fontWeight: "bold",
+  },
+  bodyOfComment: {
+    color: "black",
+    fontSize: 12,
+  },
+  seeReplies: {
+    color: "blue",
+  },
+  test: {
+    width: 20,
+    marginRight: 90
+  },
+  reply: {
+    color: "blue",
+  },
+  replieIndent: {
+    marginLeft: 25,
+  }
 });
